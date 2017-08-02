1a6e7aff95a242511e9a3c5cd4d47097997c8fc3
Jason Koh <bk7749@gmail.com>
2017-08-02 02:55:24 -0700

    update for the new citadel

--- citadel/__init__.py
+++ citadel/__init__.py
@@ -8,112 +8,39 @@ class Citadel():
 
     def __init__(self, base_url):
         self.base_url = base_url
-
-    def api_url(self):
-        return '{0}/api'.format(self.base_url)
+        self.api_url = '{0}/api'.format(self.base_url)
+        self.headers = {
+            'content-type': 'application/json',
+        }
 
     def create_point(self, metadata):
-        resp = requests.post(self.api_url() + '/point/', json=metadata)
+        resp = requests.post(self.api_url() + '/point/', 
+                             json=metadata, 
+                             headers=headers)
+        if resp.statuc_code in [400, 500]:
+            #TODO: define better behavior
+            return False
+        resp = resp.json()
         if resp.status_code!=201:
             return {'success': False, 
-                    'result':{
-                        'reason': resp.text
-                        }
-                    }
+                    'reason': resp['reason']
+                   }
         else:
-            return {'success': True, 
-                    'result':{
-                        'uuid': resp.json()['uuid']
-                        }
-                    }
+            return resp['uuid']
 
-    def query_points(self, name=None, tag_query=None, geo_query=None):
-        params = dict()
-        if name:
-            params['name'] = name
-        if tag_query:
-            if not isinstance(tag_query,str):
-                tag_query = json.dumps(tag_query)
-            params['tag_query'] = tag_query
-        if geo_query:
-            if not isinstance(geo_query,str):
-                geo_query = json.dumps(geo_query)
-            params['geo_query'] = geo_query
+    def query_points(self, query):
+        #TODO
 
-        resp = requests.get(self.api_url() + '/point', params=params)
-        if resp.status_code==200:
-            found_point = resp.json()['point_list']
-            return {'success': True, 
-                    'result':{
-                        'point_list':found_point
-                        }
-                    }
-        else:
-            return {'success': False, 
-                    'result':{
-                        'reason': resp.text
-                        }
-                    }
 
     def delete_point(self, uuid):
-        # delete the uuid
-        resp = requests.delete('{0}/point/{1}'.format(self.api_url(), uuid))
-        if resp.status_code==200:
-            return {'success': True}
-        else:
-            return {'success': False,
-                    'result':{
-                        'reason': resp.text
-                        }
-                    }
+        #TODO
 
-    def put_timeseries(self, uuid, ts_data):
-        data = {'samples': ts_data}
-        ts_url = '{0}/point/{1}/timeseries'.format(self.api_url(), uuid)
-        resp = requests.post(ts_url, json=data)
-        if resp.status_code==200:
-            return {'success':True}
-        else:
-            return {'success':False,
-                    'result':{
-                        'reason': resp.text
-                        }
-                    }
+    def post_data(data, headers=headers):
+        resp = requests.post(data_url, json={'data': data}, headers=headers)
+        return resp.json()['success']
 
     def get_timeseries(self, uuid, start_time=None, end_time=None):
-        params = dict()
-        if start_time:
-            params['start_time'] = str(int(start_time))
-        if end_time:
-            params['end_time'] = str(int(end_time))
-
-        ts_url = '{0}/point/{1}/timeseries'.format(self.api_url(), uuid)
-        resp = requests.get(ts_url, params=params)
-        if resp.status_code==200:
-            return {'success': True,
-                    'result':{
-                        'data': resp.json()['data']
-                        }
-                    }
-        else:
-            return {'success': False,
-                    'result':{
-                        'reason': resp.text
-                        }
-                    }
+        # TODO
 
     def delete_timeseries(self, uuid, start_time, end_time):
-        ts_url = '{0}/point/{1}/timeseries'.format(self.api_url(), uuid)
-        params = {
-                'start_time': start_time,
-                'end_time': end_time
-                }
-        resp = requests.delete(ts_url, params=params)
-        if resp.status_code==200:
-            return {'success':True}
-        else:
-            return {'success':False,
-                    'result':{
-                        'reason': resp.text
-                        }
-                    }
+        # TODO
