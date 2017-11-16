49ec58e571b3078666a90c2e4a7cd15b2c363d0e
Jason Koh <bk7749@gmail.com>
2017-11-15 19:38:17 -0800

    update

--- citadel/__init__.py
+++ citadel/__init__.py
@@ -4,6 +4,8 @@ import time, random
 import json
 import pdb
 
+from .errors import CitadelError
+
 class Citadel():
 
     def __init__(self, base_url):
@@ -13,6 +15,15 @@ class Citadel():
             'content-type': 'application/json',
         }
 
+    def get_point(self, uuid, headers=None):
+        if not headers:
+            headers = self.headers
+        resp = requests.get(self.api_url + '/point/{0}'.format(uuid),
+                            headers=headers)
+        if resp.status_code in [400, 500]:
+            raise CitadelError(resp)
+        return resp.json()['results']
+
     def create_point(self, metadata, headers=None):
         if not headers:
             headers = self.headers
@@ -20,8 +31,7 @@ class Citadel():
                              json=metadata, 
                              headers=self.headers)
         if resp.status_code in [400, 500]:
-            #TODO: define better behavior
-            return False
+            raise CitadelError(resp)
         resp_json = resp.json()
         if resp.status_code!=201:
             return {'success': False, 
@@ -36,25 +46,42 @@ class Citadel():
         query = {'query': query}
         query_url = self.api_url + '/query'
         resp = requests.post(query_url, json=query, headers=headers)
+        if resp.status_code in [400, 500]:
+            raise CitadelError(resp)
         points = resp.json()['results']
         return points
-
-    def delete_point(self, uuid):
-        #TODO
-        pass
 
     def post_data(self, data, headers=None):
         if not headers:
             headers = self.headers
         data_url = self.api_url + '/data'
         resp = requests.post(data_url, json={'data': data}, headers=headers)
-        if resp.status_code in [200, 201]:
+        if resp.status_code in [400, 500]:
+            raise CitadelError(resp)
+        elif resp.status_code in [200, 201]:
             return resp.json()['success']
         else:
             return False
 
-    def get_timeseries(self, uuid, start_time=None, end_time=None):
-        # TODO
+    def query_data(self, uuid, start_time=None, end_time=None,
+                       headers=None):
+        if not headers:
+            headers = self.headers
+        query = {'query': {}}
+        if start_time:
+            query['query']['timestamp_min'] = start_time
+        if end_time:
+            query['query']['timestamp_max'] = end_time
+        resp = requests.post(self.api_url + '/querydata',
+                             json=query, 
+                             headers=self.headers)
+        if resp.status_code in [400, 500]:
+            raise CitadelError(resp)
+        return resp.json()['results']
+
+
+    def query_bbox(self, begin_time, end_time, min_lng,
+                   min_lat, max_lng, max_lat):
         pass
 
     def delete_timeseries(self, uuid, start_time, end_time):
--- test.py
+++ test.py
@@ -1,4 +1,54 @@
 from citadel import Citadel
+import json
+import arrow
+
+import pdb
+
+with open('config/citadel_config.json', 'r') as fp:
+    config = json.load(fp)
+
+citadel = Citadel(config['hostname'])
+
+# Create Test
+point = {
+    'name': 'test_sensor',
+    'unit': 'ppm',
+    'pointType': 'air_quality'
+}
+
+#res = citadel.create_point(point)
+#print(res)
+
+# Query Test
+res = citadel.query_points({
+    'unit': 'ppm'
+})
+print(res)
+uuid = res[0]
+
+# Get Point Test
+res = citadel.get_point(uuid)
+print(res)
 
 
-citadel = Citadel('http://citadel.ucsd.edu')
+# Post Data Test
+t = 1510791536000
+data = [
+    {
+        'geometryType': 'point',
+        'coordinates': [[-117.0, 33.0]],
+        'uuid': uuid,
+        'value': 90,
+        'timestamp': t
+    }
+]
+resp = citadel.post_data(data)
+assert resp
+
+# Get Data from a UUID
+data = citadel.query_data(uuid, None, None)
+pdb.set_trace()
+
+
+
+
