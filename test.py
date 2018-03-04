d3df76cea9b2bfd827eabdccc1293a906f9147f8
Jason Koh <bk7749@gmail.com>
2018-03-03 16:20:52 -0800

    update

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import pdb
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-with open('config/citadel_config.json', 'r') as fp:
+with open('config/citadel_config_local.json', 'r') as fp:
     config = json.load(fp)
 apikey = config['apikey']
 
@@ -33,16 +33,17 @@ except Exception as e:
 res = citadel.query_points({
     'name': point['name']
 })
-uuid = res[0]
-if uuid:
+if res:
+    uuid = res[0]
     print('Success: Point query')
 else:
-    print('Failed: Point query due to {0}')
+    print('Failed: Empty point query')
+    sys.exit()
 
 # Get Point Test
 res = citadel.get_point(uuid)
 if uuid:
-    print('Success: Get point')
+    print('Success: Get point', res)
 else:
     print('Failed: Get point')
 
@@ -61,20 +62,26 @@ else:
 # Get Data from a UUID
 data = citadel.get_data(uuid, None, None)
 if data:
+    print(data)
     print("Success: retrieve data only with a UUID")
 else:
     print('Failed: empty result')
 
 begin_time = t - 1000000
 end_time = t + 1000000
-min_lng = -120
-min_lat = 30
-max_lng = -110
-max_lat = 35
-uuids = []
+#min_lng = -120
+#min_lat = 30
+#max_lng = -110
+#max_lat = 35
+min_lng = -117.0848
+max_lng = -116.98368
+min_lat = 32.621090
+max_lat = 33.623620
+uuids = [uuid]
 res = citadel.query_bbox(begin_time, end_time, min_lng, 
                          min_lat, max_lng, max_lat, uuids)
 if res:
+    print(res)
     print("Success: retrieve data with BBOX query")
 else:
     print('Failed')
